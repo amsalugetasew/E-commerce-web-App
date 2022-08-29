f508df13087824fbc4d4c2e4df83e5c257f84a11
unknown <amsalugetasew>
2022-08-29 04:28:56 -0700

    my build updation

--- gemart/src/Components/ProductDetail.jsx
+++ gemart/src/Components/ProductDetail.jsx
@@ -8,9 +8,9 @@ import { addCart, delCart } from '../redux/action/index'
 // import {DATA} from '../Data'
 var data = [{
     "id": 1,
-    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    "title": "Jano",
     "price": 109.95,
-    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    "description": "Amhara Men traditional clothing for Gondar, Gojam, and Shewa",
     "category": "men's clothing",
     "image": require('../Men/m1.png'),
     
@@ -22,9 +22,9 @@ var data = [{
 },
 {
     "id": 2,
-    "title": "Mens Casual Premium Slim Fit T-Shirts ",
+    "title": "Gondar Couples cloths",
     "price": 22.3,
-    "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+    "description": "Couples Gondar Cloths for sale and rent",
     "category": "men's clothing",
     "image": require('../Men/m2.jpg'),
     "rating":
@@ -35,9 +35,9 @@ var data = [{
 },
 {
     "id": 3,
-    "title": "Mens Cotton Jacket",
+    "title": "Tradditional Amhara clothing",
     "price": 55.99,
-    "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+    "description": "Tradditional Amhara clothing",
     "category": "men's clothing",
     "image": require('../Men/m3.jpg'),
     "rating":
@@ -48,9 +48,9 @@ var data = [{
 },
 {
     "id": 4,
-    "title": "Mens Casual Slim Fit",
+    "title": "Mens Epiphany group cloth",
     "price": 15.99,
-    "description": "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+    "description": "Gondar Ethiopia men's dressed in traditional attire with pilgrim rod celebrate Epiphany/Timket festival",
     "category": "men's clothing",
     "image": require('../Men/m4.jpg'),
     "rating":
@@ -191,17 +191,17 @@ var data = [{
 },
 {
     "id": 15,
-    "title": "BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
+    "title": "Gondar Women's cloth and interesting Mekenet",
     "price": 56.99,
-    "description": "Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates", "category": "women's clothing",
+    "description": "Gondar Women's cloth and interesting Mekenet with red, green, yellow, and black",
     "image": require('../Women/m1.jpg'),
     "rating": { "rate": 2.6, "count": 235 }
 },
 {
     "id": 16,
-    "title": "Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket",
+    "title": "Gondar Women's cloth and interesting Mekenet",
     "price": 29.95,
-    "description": "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
+    "description": "Gondar Women's cloth and interesting Mekenet with red, green, yellow, and black",
     "category": "women's clothing",
     "image": require('../Women/m2.jpg'),
     "rating":
@@ -212,9 +212,9 @@ var data = [{
 },
 {
     "id": 17,
-    "title": "Rain Jacket Women Windbreaker Striped Climbing Raincoats",
+    "title": "Gondar Women's cloth and interesting Mekenet",
     "price": 39.99,
-    "description": "Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.", "category": "women's clothing",
+    "description": "Gondar Women's cloth and interesting Mekenet with red, green, yellow, and black",
     "image": require('../Women/m3.jpg'),
     "rating":
     {
@@ -224,9 +224,9 @@ var data = [{
 },
 {
     "id": 18,
-    "title": "MBJ Women's Solid Short Sleeve Boat Neck V ",
+    "title": "Gondar Women's cloth and interesting Mekenet",
     "price": 9.85,
-    "description": "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem",
+    "description": "Gondar Women's cloth and interesting Mekenet with red, green, yellow, and black",
     "category": "women's clothing",
     "image": require('../Women/m4.jpg'),
     "rating":
@@ -237,9 +237,9 @@ var data = [{
 },
 {
     "id": 19,
-    "title": "Opna Women's Short Sleeve Moisture",
+    "title": "Gondar Women's cloth and interesting Mekenet",
     "price": 7.95,
-    "description": "100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort",
+    "description": "Gondar Women's cloth and interesting Mekenet with red, green, yellow, and black",
     "category": "women's clothing",
     "image": require('../Women/m5.jpg'),
     "rating":
@@ -250,9 +250,9 @@ var data = [{
 },
 {
     "id": 20,
-    "title": "DANVOUY Womens T Shirt Casual Cotton Short",
+    "title": "Gondar Women's cloth with red Mekenet",
     "price": 12.99,
-    "description": "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
+    "description": "Gondar Women's cloth with red Mekenet",
     "category": "women's clothing",
     "image": require('../Women/m6.jpg'),
     "rating":
