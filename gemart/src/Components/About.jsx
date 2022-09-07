05680e48249fc9111b015658947e45b3f7c53dfd
unknown <amsalugetasew>
2022-09-07 03:28:36 -0700

    Add Product Interface

--- gemart/src/Components/About.jsx
+++ gemart/src/Components/About.jsx
@@ -7,10 +7,16 @@ function About() {
         <div className="container d-flex py-4 my-5 justify-content-center">
             <div className="row">
                 <h1 className='text-primary fw-bold'>About Us</h1>
-                <p className="lead me-5">
-                 The University of Gondar, until 2003 known as the Gondar College of Medical Sciences, is the oldest medical school in Ethiopia. Established as the Public Health College in 1954, it is located in Gondar, the former capital of Ethiopia. In 2010, the university offered 42 undergraduate and 17 postgraduate programs.
+                <p className="lead me-5" id='abcontent'>
+                Gondar, or Gonder, is a city in northern Ethiopia. 
+                It's known for the walled Fasil Ghebbi fortress and palace compound, 
+                once the seat of Ethiopian emperors. Dominating it is the immense 
+                17th-century castle of Emperor Fasilides, which combines Portuguese, 
+                Indian and local architectural styles. Outside the complex is Debre
+                 Berhan Selassie church, with an interior of elaborate murals, 
+                 including a ceiling of faces.
                 </p>
-                <NavLink to="/contact" className="btn btn-outline-primary px-3 w-25">Contact Us</NavLink>
+                <NavLink to="/contact" className="btn btn-outline-primary btn-dark text-white">Contact Us</NavLink>
             </div>
             <div className="col-md-6 mx-5">
                 <img src={Aboutus} alt="" height="400px" width="400px" />
