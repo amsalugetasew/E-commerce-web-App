86edc82b228f3fe39397cae4fff28d07139e3140
unknown <amsalugetasew>
2022-09-07 03:12:51 -0700

    Add Product Interface

--- gemart/src/Components/NavBar.jsx
+++ gemart/src/Components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
     const state = useSelector((state) => state.handleCart)
     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-white py-3 shadow-sm">
+            <nav className="navbar navbar-expand-lg bg-dark py-3  shadow-sm">
                 <div className="container">
                     <NavLink className="navbar-brand fw-bold fs-4" to="/">
                         {/* Habesha Collection */}
@@ -17,27 +17,27 @@ function NavBar() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+                                <NavLink className="nav-link text-white active" aria-current="page" to="/">Home</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" to="/Products">Product</NavLink>
+                                <NavLink className="nav-link text-white" to="/Products">Product</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" to="/about">About</NavLink>
+                                <NavLink className="nav-link text-white" to="/about">About</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" to="/contact">Contact</NavLink>
+                                <NavLink className="nav-link text-white" to="/contact">Contact</NavLink>
                             </li>
                             </ul>
                             <div className="buttons">
                                 <NavLink to="/sign-in" className="btn btn-outline-dark ms-2">
-                                    <i className="fa fa-sign-in me-1">Login</i> 
+                                    <i className="fa fa-sign-in me-1 text-white">Login</i> 
                                 </NavLink>
                                 <NavLink to="/sing-up" className="btn btn-outline-dark ms-2">
-                                    <i className="fa fa-user-plus me-1">Register</i> 
+                                    <i className="fa fa-user-plus me-1 text-white">Register</i> 
                                 </NavLink>
                                 <NavLink to="/cart" className="btn btn-outline-dark ms-2">
-                                    <i className="fa fa-shopping-cart me-1 fa-bold">Cart ({state.length})</i>                                     
+                                    <i className="fa fa-shopping-cart me-1 fa-bold text-white">Cart ({state.length})</i>                                     
                                 </NavLink>
                                 {/* <i class="fa-solid fa-user">x</i> */}
                             </div>
